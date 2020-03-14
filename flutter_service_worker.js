a501fbc526e4a8859df9c31f4e0cb84d5d158fb3
Y.Fujiura <memory.loverz@gmail.com>
2020-03-15 00:12:32 +0900

    v0.0.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "d652db39e99cc5bea8a3017d46c6eb10",
-"/main.dart.js": "0ac9a1062e8da42cc1e615904ae11629",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/manifest.json": "54f520408706501ccdf97a573065bbd9",
+  "/index.html": "c5f5a392ef13a09fade9e16342845652",
+"/main.dart.js": "5b62b5f99737fc3fe3d226c22eae5971",
+"/icons/safari-icon.svg": "5a55a267965e405626e1411672503c4d",
+"/icons/ogp.png": "25c4c0b8c614fc6460e0ba3c2fbf5b8c",
+"/icons/mstile-144x144.png": "21afa91728f922eb34d5b41f1b835ab5",
+"/icons/favicon.ico": "2c42ff5cc4d3b0062958ed6d0cd1105c",
+"/icons/android-chrome-192x192.png": "ff96ff8f8b075a629d7d5315721739d5",
+"/icons/apple-touch-icon-180x180.png": "9e153ca4312a0c4774a34dc7fb9b267d",
+"/icons/android-chrome-512x512.png": "2186f6538a1d110a5fe9c1aaf9b39f62",
+"/manifest.json": "3bc0709f0db08fe36164ae63a77604d0",
 "/assets/LICENSE": "1a80be6c5724a31e6f9c9e06dba53b63",
 "/assets/AssetManifest.json": "28d4a33467bfc044c45b04ff1e159398",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/assets/images/ogp.png": "38c34acbeecf66c814bcf257cb3076a5",
+"/assets/assets/images/ogp.png": "25c4c0b8c614fc6460e0ba3c2fbf5b8c",
 "/assets/assets/images/good.png": "a2e2185fe47ec94f235601e9620185ef",
 "/assets/assets/images/study.png": "6b657a19a763154f6ecc1da0c03b2905",
 "/assets/assets/images/fighat.png": "bc70aedb1cd1648820cf3641d345db00"
